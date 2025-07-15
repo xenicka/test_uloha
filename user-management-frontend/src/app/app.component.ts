5c8ffaa2bf2a60cdabf0e28b00bb8a1432de46e9
Kseniia Velykodna <kseni.dav@gmail.com>
2025-07-15 09:55:25 +0200

    translation

--- user-management-frontend/src/app/app.component.ts
+++ user-management-frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { UserDetailComponent } from './components/user-detail/user-detail.component';
 import { UserTableComponent } from './components/user-table/user-table.component';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,7 @@ import { UserTableComponent } from './components/user-table/user-table.component
 })
 export class AppComponent {
   title = 'user-management-frontend';
+  constructor(private titleService: Title) {
+    this.titleService.setTitle($localize`${this.title}`);
+  }
 }
--- user-management-frontend/src/app/components/navbar/navbar.component.ts
+++ user-management-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule, PRECONNECT_CHECK_BLOCKLIST } from '@angular/common';
-import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +7,4 @@ import { Title } from '@angular/platform-browser';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
-  title = 'got it';
-  constructor(private titleService: Title) {
-    this.titleService.setTitle($localize`${this.title}`);
-  }
-}
+export class NavbarComponent {}
