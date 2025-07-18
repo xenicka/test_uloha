0fb3a84b8aab373715cd49503fed8163c47e00b9
Kseniia Velykodna <kseni.dav@gmail.com>
2025-07-18 14:38:25 +0200

    added css

--- user-management-frontend/src/app/components/user-detail/user-detail.component.ts
+++ user-management-frontend/src/app/components/user-detail/user-detail.component.ts
@@ -2,10 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-detail',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.css',
 })
@@ -13,6 +14,7 @@ export class UserDetailComponent implements OnInit {
   userId!: number;
   isViewForm = false;
   user: any; // or your User interface type
+  isAddParamForm = false;
 
   constructor(private htttp: HttpClient, private route: ActivatedRoute) {}
 
@@ -26,9 +28,14 @@ export class UserDetailComponent implements OnInit {
       .get(`http://localhost:8080/api/users/${id}`)
       .subscribe((data) => {
         this.user = { ...data };
+        console.log(data);
       });
   }
   view() {
     this.isViewForm = !this.isViewForm;
   }
+  openParameterForm() {
+    this.isAddParamForm = true;
+  }
+  addParameter() {}
 }
